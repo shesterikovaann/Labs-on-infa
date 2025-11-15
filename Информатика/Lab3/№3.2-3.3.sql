711d2dcebd0532bbd30242d5c6ed4193fdc458b4
Анна Шестерикова <151235567+shesterikovaann@users.noreply.github.com>
2025-11-15 08:48:06 +0300

    Update №3.2-3.3.sql

--- Информатика/Lab3/№3.2-3.3.sql
+++ Информатика/Lab3/№3.2-3.3.sql
@@ -1,1 +1,20 @@
+CREATE TABLE tasks (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR,
+  priority VARCHAR,
+  due_date INTEGER,
+  status VARCHAR
 
+);
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR
+);
+
+CREATE TABLE task_assignments (
+  assignment_id INTEGER PRIMARY KEY,
+  task_id INTEGER,
+  user_id INTEGER,
+  FOREIGN KEY (task_id) REFERENCES tasks (id),
+  FOREIGN KEY (user_id) REFERENCES users (id)
+)
